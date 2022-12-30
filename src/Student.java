bc484d7998c86a0d1fbe7900ff40de873221a53d
SuzukiNaoto0422 <naojin.naojin@gmail.con>
2022-12-30 19:38:24 +0900

    第4回課題訂正 （3回目） インデント修正

--- src/Student.java
+++ src/Student.java
@@ -20,9 +20,8 @@ public class Student {
     //「toString」メソッドをオーバーライドして、それらの値を含む文字列を返すようにする。
     //「toString」メソッドは、オブジェクトを文字列に変換するメソッド。Javaでは、オブジェクトを直接出力する場合、
     //「toString」メソッドが呼び出され、その戻り値が出力。
-
     @Override
     public String toString() {
         return "Student(名前：" + name + ", 身長：" + height + ". 血液型:" + bloodType +"型)";
-        }
+    }
 }
